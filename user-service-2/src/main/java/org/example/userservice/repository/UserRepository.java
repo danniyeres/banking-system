9678a72b8b0000df08ea2670ce2d690bf377a14f
don <eleusizdaniyar777@gmail.com>
2025-02-02 13:26:33 +0500

    fix

--- user-service-2/src/main/java/org/example/userservice/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.userservice.repository;
-
-import org.example.userservice.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findUserById(long id);
-    User findByEmail(String email);
-}
